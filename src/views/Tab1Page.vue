7e591eb4c581beb7eb59f4a6f5a7b0e40888a437
Aldy Syachranie <aldysyach17@gmail.com>
2022-11-01 23:19:33 +0700

    test

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,29 +1,222 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-content :fullscreen="true">
+            <h1 id="judul">Home</h1>
+            <ion-grid>
+                <ion-row>
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-title>Kolam</ion-card-title>
+                                <ion-card-subtitle>10</ion-card-subtitle>
+                            </ion-card-header>
+                        </ion-card>
+                    </ion-col>
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-title>Kolam Aktif</ion-card-title>
+                                <ion-card-subtitle>10</ion-card-subtitle>
+                            </ion-card-header>
+                        </ion-card></ion-col
+                    >
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-title>Ikan Hidup</ion-card-title>
+                                <ion-card-subtitle
+                                    >182
+                                    <span id="subsub"
+                                        >Ekor</span
+                                    ></ion-card-subtitle
+                                >
+                            </ion-card-header>
+                        </ion-card></ion-col
+                    >
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-title>Ikan Mati</ion-card-title>
+                                <ion-card-subtitle
+                                    >89
+                                    <span id="subsub"
+                                        >Ekor</span
+                                    ></ion-card-subtitle
+                                >
+                            </ion-card-header>
+                        </ion-card></ion-col
+                    >
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-title>Panen 2022</ion-card-title>
+                                <ion-card-subtitle
+                                    >109
+                                    <span id="subsub"
+                                        >Kg</span
+                                    ></ion-card-subtitle
+                                >
+                            </ion-card-header>
+                        </ion-card></ion-col
+                    >
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-title>Total Pakan</ion-card-title>
+                                <ion-card-subtitle
+                                    >223
+                                    <span id="subsub"
+                                        >Kg</span
+                                    ></ion-card-subtitle
+                                >
+                            </ion-card-header>
+                        </ion-card></ion-col
+                    >
+                </ion-row>
+            </ion-grid>
+            <h1 id="judul">TOTAL BERAT IKAN</h1>
+            <ion-grid>
+                <ion-row>
+                    <ion-col size="6">
+                        <ion-card color="dark">
+                            <img
+                                alt="Silhouette of mountains"
+                                src="/assets/nila.png"
+                            />
+                            <ion-card-header>
+                                <ion-card-title style="font-weight: bold"
+                                    >LELE</ion-card-title
+                                >
+                                <ion-card-title style="color: #2c96f1"
+                                    >200 Kg</ion-card-title
+                                >
+                            </ion-card-header>
+                        </ion-card>
+                    </ion-col>
+                    <ion-col size="6" style="margin-left: 0rem">
+                        <ion-card color="dark">
+                            <img
+                                alt="Silhouette of mountains"
+                                src="/assets/nila.png"
+                                style="height: rem"
+                            />
+                            <ion-card-header>
+                                <ion-card-title style="font-weight: bold"
+                                    >NILA MERAH</ion-card-title
+                                >
+                                <ion-card-title style="color: #2c96f1"
+                                    >200 Kg</ion-card-title
+                                >
+                            </ion-card-header>
+                        </ion-card></ion-col
+                    >
+                </ion-row>
+            </ion-grid>
+            <h1 id="judul">KUALITAS AIR</h1>
+            <ion-grid>
+                <ion-row>
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-subtitle>pH</ion-card-subtitle>
+                                <ion-card-title
+                                    >Normal
+                                    <span style="color: #2c96f1"
+                                        >6 Kolam</span
+                                    ></ion-card-title
+                                >
+                                <ion-card-title
+                                    >Abnormal
+                                    <span style="color: #2c96f1"
+                                        >6 Kolam</span
+                                    ></ion-card-title
+                                >
+                            </ion-card-header>
+                        </ion-card>
+                    </ion-col>
+                    <ion-col size="6" size-md="4" size-lg="2">
+                        <ion-card color="dark">
+                            <ion-card-header>
+                                <ion-card-subtitle>DO</ion-card-subtitle>
+                                <ion-card-title
+                                    >Normal
+                                    <span style="color: #2c96f1"
+                                        >6 Kolam</span
+                                    ></ion-card-title
+                                >
+                                <ion-card-title
+                                    >Abnormal
+                                    <span style="color: #2c96f1"
+                                        >6 Kolam</span
+                                    ></ion-card-title
+                                >
+                            </ion-card-header>
+                        </ion-card>
+                    </ion-col>
+                </ion-row>
+            </ion-grid>
+            <div id="container"></div>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonContent, IonPage } from "@ionic/vue";
+import { defineComponent } from "vue";
 
-export default  defineComponent({
-  name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+export default defineComponent({
+    name: "HomePage",
+    components: {
+        IonContent,
+        IonPage,
+    },
 });
 </script>
+
+<style scoped>
+#container {
+    text-align: center;
+
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 50%;
+    transform: translateY(-50%);
+}
+
+#container strong {
+    font-size: 20px;
+    line-height: 26px;
+}
+
+#container p {
+    font-size: 16px;
+    line-height: 22px;
+
+    color: #8c8c8c;
+
+    margin: 0;
+}
+
+#container a {
+    text-decoration: none;
+}
+
+ion-card-subtitle {
+    font-size: 2rem;
+    font-weight: bold;
+}
+
+#subsub {
+    font-size: 1.5rem;
+    font-weight: normal;
+}
+
+#judul {
+    margin-left: 1rem;
+}
+
+#hero {
+    font-family: 0.5rem;
+}
+</style>
--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,29 +1,88 @@
 <template>
-  <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 2</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 2</ion-title>
-        </ion-toolbar>
-      </ion-header>
-      
-      <ExploreContainer name="Tab 2 page" />
-    </ion-content>
-  </ion-page>
+    <ion-page>
+        <ion-content :fullscreen="true">
+            <h1 id="judul">Home</h1>
+            <ion-list :inset="true">
+                <ion-item>
+                    <ion-card color="dark">
+                        <ion-card-header>
+                            <ion-grid>
+                                <ion-row style="border-bottom: 2px solid black">
+                                    <ion-col>Kolam</ion-col>
+                                    <ion-col class="ion-text-end"
+                                        ><ion-badge>Aktif</ion-badge></ion-col
+                                    >
+                                </ion-row>
+                                <ion-row>
+                                    <ion-col>01-09-2022</ion-col>
+                                </ion-row>
+                                <ion-row>
+                                    <ion-col>01-09-2022</ion-col>
+                                </ion-row>
+                                <ion-row>
+                                    <ion-col>01-09-2022</ion-col>
+                                </ion-row>
+                            </ion-grid>
+
+                            <!-- <ion-card-title>Kolam</ion-card-title>
+                            <ion-card-subtitle>10</ion-card-subtitle> -->
+                        </ion-card-header>
+                    </ion-card>
+                </ion-item>
+                <ion-item>
+                    <ion-card color="dark">
+                        <ion-card-header>
+                            <ion-card-title>Kolam</ion-card-title>
+                            <ion-card-subtitle>10</ion-card-subtitle>
+                        </ion-card-header>
+                    </ion-card>
+                </ion-item>
+                <ion-item>
+                    <ion-card color="dark">
+                        <ion-card-header>
+                            <ion-card-title>Kolam</ion-card-title>
+                            <ion-card-subtitle>10</ion-card-subtitle>
+                        </ion-card-header>
+                    </ion-card>
+                </ion-item>
+                <ion-item>
+                    <ion-card color="dark">
+                        <ion-card-header>
+                            <ion-card-title>Kolam</ion-card-title>
+                            <ion-card-subtitle>10</ion-card-subtitle>
+                        </ion-card-header>
+                    </ion-card>
+                </ion-item>
+                <ion-item>
+                    <ion-card color="dark">
+                        <ion-card-header>
+                            <ion-card-title>Kolam</ion-card-title>
+                            <ion-card-subtitle>10</ion-card-subtitle>
+                        </ion-card-header>
+                    </ion-card>
+                </ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { defineComponent } from "vue";
+import { IonPage, IonContent } from "@ionic/vue";
 
 export default defineComponent({
-  name: 'Tab2Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+    name: "Tab2Page",
+    components: {
+        IonContent,
+        IonPage,
+    },
 });
 </script>
+<style scoped>
+#judul {
+    margin-left: 1rem;
+}
+ion-card {
+    width: 100%;
+}
+</style>
